101e18452cdb46df3da1674cff2f0518cfa28d74
Michael Olorundare <michaelolorundare@gmail.com>
2026-01-20 11:42:52 +0100

    resolution of asset display error

--- lib/src/constants/app_assets.dart
+++ lib/src/constants/app_assets.dart
@@ -1,41 +1,40 @@
 class AppAssets {
   // PNG Assets
   static const String imateStores1Png =
-      'assets/images/CredPal-Assets/CredPal-Pngs/IMate_Stores_1.png';
+      'assets/images/IMate_Stores_1.png';
   static const String iPhonePng =
-      'assets/images/CredPal-Assets/CredPal-Pngs/iPhone.png';
+      'assets/images/iPhone.png';
   static const String nokiaG20Png =
-      'assets/images/CredPal-Assets/CredPal-Pngs/nokia_g20.png';
+      'assets/images/nokia_g20.png';
   static const String ogabassey1Png =
-      'assets/images/CredPal-Assets/CredPal-Pngs/ogabassey_1.png';
+      'assets/images/ogabassey_1.png';
   static const String okayFonesPng =
-      'assets/images/CredPal-Assets/CredPal-Pngs/okay_fones.png';
+      'assets/images/okay_fones.png';
   static const String speakerPng =
-      'assets/images/CredPal-Assets/CredPal-Pngs/speaker.png';
+      'assets/images/speaker.png';
   static const String xsPng =
-      'assets/images/CredPal-Assets/CredPal-Pngs/xs.png';
-
+      'assets/images/xs.png';
   // SVG Assets
   static const String casperSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/casper.svg';
+      'assets/images/casper.svg';
   static const String dreamworksSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/dreamworks.svg';
+      'assets/images/dreamworks.svg';
   static const String hubmartSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/hubmart.svg';
+      'assets/images/hubmart.svg';
   static const String justFonesSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/just_fones.svg';
+      'assets/images/just_fones.svg';
   static const String justRiteSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/just_rite.svg';
+      'assets/images/just_rite.svg';
   static const String justUsedSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/just_used.svg';
+      'assets/images/just_used.svg';
   static const String ogaBasseySvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/oga_bassey.svg';
+      'assets/images/oga_bassey.svg';
   static const String orileRestaurantSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/orile_restaurant.svg';
+      'assets/images/orile_restaurant.svg';
   static const String pointekSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/pointek.svg';
+      'assets/images/pointek.svg';
   static const String scanSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/scan.svg';
+      'assets/images/scan.svg';
   static const String slotSvg =
-      'assets/images/CredPal-Assets/CredPal-svgs/slot.svg';
+      'assets/images/slot.svg';
 }
