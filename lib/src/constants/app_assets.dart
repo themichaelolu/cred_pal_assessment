faef1321abc2cb59bcfc1e3b50343e9ba0d8a7da
Michael Olorundare <michaelolorundare@gmail.com>
2026-01-20 10:21:14 +0100

    addition of app constants

---
+++ lib/src/constants/app_assets.dart
@@ -0,0 +1,41 @@
+class AppAssets {
+  // PNG Assets
+  static const String imateStores1Png =
+      'assets/images/CredPal-Assets/CredPal-Pngs/IMate_Stores_1.png';
+  static const String iPhonePng =
+      'assets/images/CredPal-Assets/CredPal-Pngs/iPhone.png';
+  static const String nokiaG20Png =
+      'assets/images/CredPal-Assets/CredPal-Pngs/nokia_g20.png';
+  static const String ogabassey1Png =
+      'assets/images/CredPal-Assets/CredPal-Pngs/ogabassey_1.png';
+  static const String okayFonesPng =
+      'assets/images/CredPal-Assets/CredPal-Pngs/okay_fones.png';
+  static const String speakerPng =
+      'assets/images/CredPal-Assets/CredPal-Pngs/speaker.png';
+  static const String xsPng =
+      'assets/images/CredPal-Assets/CredPal-Pngs/xs.png';
+
+  // SVG Assets
+  static const String casperSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/casper.svg';
+  static const String dreamworksSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/dreamworks.svg';
+  static const String hubmartSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/hubmart.svg';
+  static const String justFonesSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/just_fones.svg';
+  static const String justRiteSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/just_rite.svg';
+  static const String justUsedSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/just_used.svg';
+  static const String ogaBasseySvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/oga_bassey.svg';
+  static const String orileRestaurantSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/orile_restaurant.svg';
+  static const String pointekSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/pointek.svg';
+  static const String scanSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/scan.svg';
+  static const String slotSvg =
+      'assets/images/CredPal-Assets/CredPal-svgs/slot.svg';
+}
---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff274FED);
+  static const backGroundColor = Color(0xffD0DAFF);
+}
